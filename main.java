3912b5e1dc59344e7ef9ace22f09e77635160996
PrabhulingBiradar <prabulingabiradar537.xworkz@gmail.com>
2021-12-21 11:05:26 +0530

    learning mainjava

---
+++ main.java
@@ -0,0 +1,20 @@
+class main{
+public static void main(String[]args){
+
+
+short[] studentid={22,52,55,33,89,91,26,25,97,54};
+   System.out.println(studentid[0]);
+   System.out.println(studentid[1]);
+ System.out.println(studentid[2]);
+ System.out.println(studentid[3]);
+ System.out.println(studentid[4]);
+ System.out.println(studentid[5]);
+ System.out.println(studentid[6]);
+ System.out.println(studentid[7]);
+ System.out.println(studentid[8]);
+ System.out.println(studentid[9]);
+
+
+
+}
+}
